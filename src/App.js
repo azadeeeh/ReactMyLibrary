9f096fae32e5d6887f5b83a16feceb2c208a88ec
azadeeeh <azadeeeh@yahoo.com>
2023-11-21 18:14:49 -0800

    basic component added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@
 function App() {
   return (
     <div className="App">
-      blank
+      <Welcome />
     </div>
   );
+};
+
+function Welcome() {
+  return (
+    <div className="Welcome">
+      <h1>Welcome to the BookList App</h1>
+      <p>here you can create a booklist and add or remove books from your list.</p>
+    </div>
+  )
 }
+
+
 
 export default App;
