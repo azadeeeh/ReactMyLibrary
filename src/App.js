c8d144d1a48aa8be4e3e133dc36af604f55c3b47
azadeeeh <azadeeeh@yahoo.com>
2023-11-21 19:18:10 -0800

    adding props to bookList

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import BookList from './bookList.js';
 import booklist from './bookList.js';
 
 function App() {
+  //making the BookList component to work dynamically by passing props to it
+  const books = [
+    { id: 1, title: 'Becoming' },
+    { id: 2, title: 'Hello' },
+    { id: 3, title: 'Python' },
+    { id: 4, title: 'Java Script' },
+
+  ];
   return (
     <div className="App">
       <Welcome />
-      <BookList />
+      <BookList books={books} />    /
     </div>
   );
 };
+
+
 
 function Welcome() {
   return (
--- src/bookList.js
+++ src/bookList.js
@@ -1,13 +1,7 @@
 
 
-const BookList = () => {
-    const books = [
-        { id: 1, title: 'Becoming' },
-        { id: 2, title: 'Hello' },
-        { id: 3, title: 'Python' },
-        { id: 4, title: 'Java Script' },
+const BookList = ({ books }) => {
 
-    ];
 
     const bookList = books.map(book =>
         <li key={book.id}>
